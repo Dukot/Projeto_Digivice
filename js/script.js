e7c159701e16c585eb9a483ebab8197b00587cab
Bernardo Grabowski Costa <96502871+Dukot@users.noreply.github.com>
2023-05-11 23:25:38 -0300

    incluindo função dark theme

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const switcher = document.querySelector('#botao-alterar-tema');
+
+switcher.addEventListener('click', () => {
+  document.body.classList.toggle('modo-escuro');
+
+  const className = document.body.className;
+    if (className === '') {
+        this.textContent = 'modo-escuro';
+    } else {
+        
+    } 
+    console.log(className);
+});
